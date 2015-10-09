5aec724a35a0c30c65094489d40c1a32068847d4
jdpigeon <jdpigeon@hotmail.com>
2015-10-09 11:24:07 -0400

    Removed NeuN count reliance and added comments on functions

--- CellCountAnalysis.py
+++ CellCountAnalysis.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
+'''
 Created on Mon Jul 27 12:52:11 2015
 
 Cell Count Analysis:
@@ -13,7 +13,7 @@ Each folder stores 3-8 sections for each LA from both the L and R hemisphere.
 Naming convention: 
 function = snake_case
 variable = CamelCase
-"""
+'''
 import pandas as pd
 import numpy as np
 import seaborn as sns
@@ -21,18 +21,22 @@ import os
 
 pd.set_option('display.precision', 5)
 
-# Calculates the threshold of a set of cells by averaging
-# the last 5 (control) entries
-
-
 def threshold(filename):
-
+    '''
+    Calculates the threshold of a set of cells by averaging the last 5 (control) entries
+    '''
     threshold = filename['Mean'][(len(filename.index) - 5):len(filename.index)].mean(axis=0)
     
     return threshold
     
-def amygdala_loop(filename, NeuNCells, NeuNCount, timesbaseline):
-    
+def amygdala_loop(filename, timesbaseline):
+    '''
+    Loops through amygdala sections,  constructing a DataFrame for each section
+    containing Total Cell Density, Area, Number of Cells, Mean Pixel Intensity,
+    Arc+ cell Density and Arc+ proportion values. At the end of each iteration,
+    these DataFrames are appended, producing one that represents all sections
+    taken form one hemisphere of one animal.
+    '''
     SliceList = pd.DataFrame()
     for n in np.arange(1, 8):
             if not os.path.isfile(filename % n):
@@ -41,7 +45,7 @@ def amygdala_loop(filename, NeuNCells, NeuNCount, timesbaseline):
                 columns=['Number', 'Area', 'Intensity', 'Density', 'Proportion', 'Cell Density'])
             ArcCount = pd.read_csv(filename % n, index_col=" ")
             ArcCells = ArcCount['Mean'][(ArcCount['Mean'] > (threshold(ArcCount)*timesbaseline))]
-            Slice['Cell Density'][0] = ((len(NeuNCells.index) + 1) / (NeuNCount['Area'][1] / 44696.932))*.8
+            Slice['Cell Density'][0] = 16 / 44696.932
             Slice['Area'][0] = ArcCount['Area'][1]
             Slice['Number'][0] = len(ArcCells.index) + 1
             Slice['Intensity'][0] = ArcCells.mean(axis=0) - threshold(ArcCount)
@@ -53,6 +57,13 @@ def amygdala_loop(filename, NeuNCells, NeuNCount, timesbaseline):
     return SliceList
 
 def make_tables(CombinedMice, MiceLeft, MiceRight, AntLeft, CombinedAnt, CombinedPost):
+    '''
+    Constructs data tables for average Arc Cell Density, LA Area, Arc+ proportion,
+    Pixel Intensity, Total Cell Density, Left & Right cell Density, and Anterior
+    & Posterior Cell Density for export to GraphPad
+    
+    Clunky implementation currently
+    '''
     hc = 4
     low = 20
     med = 19
@@ -125,8 +136,12 @@ def make_tables(CombinedMice, MiceLeft, MiceRight, AntLeft, CombinedAnt, Combine
             
     
 def cell_count():
+    '''
+    Loops through all folders in 'To Analyze' folder, combining average Right &
+    Left hemisphere data from all mice into one DataFrame
+    '''
     
-        # Instantiate all of the global Dataframes
+    # Instantiate all of the global Dataframes
     timesbaseline = 1.5
     MiceLeft = pd.DataFrame()
     MiceRight = pd.DataFrame()
@@ -142,11 +157,9 @@ def cell_count():
     while (i <= len(os.listdir())):
         os.chdir('%d' % i)
         Amygdala = pd.DataFrame()
-        NeuNCount = pd.read_csv('NeuNlCount.csv', index_col=" ")
-        NeuNCells = NeuNCount['Mean'][NeuNCount['Mean'] > (threshold(NeuNCount) * timesbaseline)]
 
         # Loop through slices, building up Amygdala
-        Amygdala = amygdala_loop('%dlCount.csv', NeuNCells, NeuNCount, timesbaseline)
+        Amygdala = amygdala_loop('%dlCount.csv', timesbaseline)
         MiceLeft = MiceLeft.append(Amygdala.mean(axis=0), ignore_index=True)
         
         # Seperate anterior and posterior slices
@@ -163,11 +176,9 @@ def cell_count():
     while (i <= len(os.listdir())):
         os.chdir('%d' % i)
         Amygdala = pd.DataFrame()
-        NeuNCount = pd.read_csv('NeuNrCount.csv', index_col=" ")
-        NeuNCells = NeuNCount['Mean'][(NeuNCount['Mean'] > (threshold(NeuNCount) * timesbaseline))]
 
         # Loop through slices
-        Amygdala = amygdala_loop('%drCount.csv', NeuNCells, NeuNCount, timesbaseline)
+        Amygdala = amygdala_loop('%drCount.csv', timesbaseline)
         MiceRight = MiceRight.append(Amygdala.mean(axis=0), ignore_index=True)
         
         # Seperate anterior and posterior slices
@@ -187,4 +198,4 @@ def cell_count():
 if __name__ == "__main__":
 
     Density, Proportion, Intensity, LeftDensity, CellDensity, RightDensity, Area, AntDensity, PostDensity = cell_count()
-    Area = Area /
+    
