2b3befae4eab40c6720002a5caba238517b50ac4
jdpigeon <morrisondano@gmail.com>
2016-03-03 13:59:10 -0500

    added subnuclei count file and heavily modified stereology count, changing back to a reading-folder-mechanic that uses a range of numbers instead of looking at every file

--- StereologyCount.py
+++ StereologyCount.py
@@ -22,14 +22,20 @@ def get_frame(filename, region):
     '''
     os.chdir(filename)
     framelist = []
+    indexframe = pd.DataFrame(np.zeros((0,1)),columns = ['Number'])
+    index = pd.DataFrame(np.zeros((1,1)),columns = ['Number'])
     
-    for i in os.listdir():
-        if region in i:
-            frame = pd.read_csv(i, index_col='Marker', engine='python')
+    for i in np.arange(1,24):
+        filename = str(i) + region + '.csv'
+        print(filename)
+        if os.path.isfile(filename):
+            frame = pd.read_csv(str(i)+region+'.csv', index_col='Marker', engine='python')
             framelist.append(frame)
+            index.iloc[0] = i
+            indexframe = indexframe.append(index)
     os.chdir(os.pardir)
             
-    return framelist
+    return framelist, indexframe
     
 def create_ratioframe(framelist):
     '''
@@ -39,10 +45,10 @@ def create_ratioframe(framelist):
     'Estimated Population using Mean Section Thickness','Coefficient of Error (Gundersen), m=1'])
     ratios = pd.DataFrame(np.zeros((1,5)),columns = ['Arc/Neun','Arc/DAPI','NeuN/DAPI',
     'Estimated Population using Mean Section Thickness','Coefficient of Error (Gundersen), m=1'])
-    indexframe = pd.DataFrame(np.zeros((0,1)),columns = ['Number'])
-    index = pd.DataFrame(np.zeros((1,1)),columns = ['Number'])
+    
     
     for i in framelist:
+        
         if 'colabel' in i.index.values.tolist():
             arcneun = (i['Total Markers Counted']['brdu'] / (i['Total Markers Counted']['colabel']))*100
             arcdapi = np.nan
@@ -58,24 +64,24 @@ def create_ratioframe(framelist):
             estpop = i['Estimated Population using Mean Section Thickness']['brdu']
             arcneun = np.nan
             neundapi = np.nan
-            ce = i['Coefficient of Error (Gundersen), m=1']['brdu']
+            print(i.index.values.tolist())
+            ce = 'placehold'
                 
         ratios.iloc[0] = [arcneun,arcdapi,neundapi,estpop, ce]
         ratioframe = ratioframe.append(ratios)
         
         #Stores blinded index number of each mouse by removing the final 4chars (ie. '.DAT') from filename
-        data_file = i['Data File']['brdu']
-        index.iloc[0] = [int(data_file[0:(len(data_file)-4)])]
-        indexframe = indexframe.append(index)
+        
+        
     
-    return ratioframe, indexframe
+    return ratioframe
 
-def assign_groups(key, ratioframe, indexframe):
+def assign_groups(key, ratioframe, index):
     '''
     Assigns real mouse names and groups to ratioframe based on key
     '''
     Key = pd.read_csv(key,index_col='Number')
-    ratioframe.index=indexframe['Number']
+    ratioframe.index=index['Number']
     ratioframe['Name'] = Key['Name']
     ratioframe['Group'] = Key['Group']
 
@@ -101,7 +107,9 @@ def make_tables(ratioframe):
     return ArcNeunRatio, NeunDAPIRatio, EstPop, ArcDapi
 
 if __name__ == "__main__":
-    framelist = get_frame('PV Engram Counting Data','LA')
-    ratioframe, indexframe = create_ratioframe(framelist)
-    ratioframe = assign_groups('PV Stereology Key.csv', ratioframe, indexframe)
+    framelist, index = get_frame('PV Total Proportion','BA')
+    ratioframe  = create_ratioframe(framelist)
+    ratioframe = assign_groups('PV Stereology Key.csv', ratioframe, index)
     ArcNeunRatio, NeunDAPIRatio, EstPop, ArcDapi = make_tables(ratioframe)
+    
+    ax = sns.barplot(x='Group', y='Arc/DAPI', data=ratioframe)
